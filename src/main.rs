07cd825e1dffe22f925fe374ab1388823a5d7d03
Anthony Rodgers <a.d.rodgers@bham.ac.uk>
2023-11-17 15:33:10 +0000

    Chapter 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
